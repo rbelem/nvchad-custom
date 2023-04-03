c8879b6a22a7897d33a172860293081392c3df63
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-04-03 08:43:33 -0400

    Set recent projects to be the first option in nvim startup

--- chadrc.lua
+++ chadrc.lua
@@ -14,8 +14,8 @@ M.ui = {
   nvdash = {
     load_on_startup = true,
     buttons = {
-      { "  Find File", "Spc f f", "Telescope find_files" },
       { "  Recent Sessions", "Spc s h", "SessionManager load_session" },
+      { "  Find File", "Spc f f", "Telescope find_files" },
       { "  Recent Files", "Spc f o", "Telescope oldfiles" },
       { "  Find Word", "Spc f w", "Telescope live_grep" },
       { "  Bookmarks", "Spc b m", "Telescope marks" },
