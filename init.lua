0645488af720d6e6f92b17da57f28d877ae855f6
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-07-27 13:58:29 -0400

    Identify invisible characters

--- init.lua
+++ init.lua
@@ -5,3 +5,14 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+local vim = vim
+local opt = vim.opt
+
+--
+-- Visual settings
+--
+
+-- show 'invisible' characters
+opt.listchars = "trail:·,tab:»·,nbsp:_"
+opt.list = true
