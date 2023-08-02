c92cdb1e1bf9357c88b0fe6962d93e016ba18a47
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-08-02 10:11:00 -0400

    Add flatten.nvim to handle opening nvim in nvim

--- plugins.lua
+++ plugins.lua
@@ -102,6 +102,16 @@ local plugins = {
       "zbirenbaum/copilot.lua",
     },
   },
+
+  {
+    "willothy/flatten.nvim",
+    config = true,
+    -- or pass configuration with
+    -- opts = {  }
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1001,
+  },
 }
 
 return plugins
