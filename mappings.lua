a3df8454d518a7fb3a32ecb6cf0386d6e12d104a
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-08-01 22:41:21 -0400

    Use gitsigns default keymappings

--- mappings.lua
+++ mappings.lua
@@ -27,6 +27,34 @@ M.general = {
 }
 
 -- more keybinds!
+M.gitsigns = {
+  n = {
+    ["<leader>hs"] = { "<cmd> Gitsigns stage_hunk<CR>", "Stage hunk" },
+
+    ["<leader>hr"] = { "<cmd> Gitsigns reset_hunk<CR>", "Reset hunk" },
+    ["<leader>rh"] = { "<cmd> Gitsigns reset_hunk<CR>", "" },
+
+    ["<leader>hS"] = { "<cmd> Gitsigns stage_buffer<CR>", "Stage buffer" },
+    ["<leader>hu"] = { "<cmd> Gitsigns undo_stage_hunk<CR>", "Undo stage hunk" },
+    ["<leader>hR"] = { "<cmd> Gitsigns reset_buffer<CR>", "Reset buffer" },
+
+    ["<leader>hp"] = { "<cmd> Gitsigns preview_hunk<CR>", "Preview hunk" },
+    ["<leader>ph"] = { "<cmd> Gitsigns preview_hunk<CR>", "" },
+
+    ["<leader>hb"] = { "<cmd> Gitsigns blame_line {full=true}<CR>", "Blame line" },
+    ["<leader>gb"] = { "<cmd> Gitsigns blame_line<CR>", "" },
+
+    ["<leader>tb"] = { "<cmd> Gitsigns toggle_current_line_blame<CR>", "Toggle current line blame" },
+    ["<leader>hd"] = { "<cmd> Gitsigns diffthis<CR>", "Diff this" },
+    ["<leader>hD"] = { "<cmd> Gitsigns diffthis('~')<CR>", "Diff this ('~')" },
+    ["<leader>td"] = { "<cmd> Gitsigns toggle_deleted<CR>", "Toggle deleted" },
+  },
+
+  v = {
+    ["<leader>hs"] = { "<cmd> Gitsigns stage_hunk {vim.fn.line('.'), vim.fn.line('v')}<CR>", "Stage hunk" },
+    ["<leader>hr"] = { "<cmd> Gitsigns reset_hunk {vim.fn.line('.'), vim.fn.line('v')}<CR>", "Reset hunk" },
+  },
+}
 
 M.nvimtree = {
   plugin = true,
