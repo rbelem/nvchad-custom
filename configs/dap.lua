3ec8c3adceed86ebdf79520547a846a3ee7149fe
Rodrigo Belem <rodrigo.belem@gmail.com>
2024-01-29 12:02:02 -0400

    Add the debugger adapter base

---
+++ configs/dap.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.adapters = {
+
+}
+
+M.configurations = {
+
+}
+
+return M
--- plugins.lua
+++ plugins.lua
@@ -196,6 +196,29 @@ local plugins = {
   },
 
   {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require("custom.configs.dap")
+    end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      require("dapui").setup()
+    end,
+    requires = { "mfussenegger/nvim-dap" },
+  },
+
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end,
+    requires = { "mfussenegger/nvim-dap" },
+  },
+
+  {
     "sindrets/diffview.nvim",
     cmd = "DiffviewOpen",
   },
